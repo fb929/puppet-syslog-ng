8b6b90bf5a0503f73eb2b22cc93383c113ad48d1
Grigory Efimov <grigory.efimov@gmail.com>
2023-09-15 20:55:08 +0000

    fixed install for debian

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,30 +1,34 @@
 class syslog_ng::install (
-  Hash $packages,
-  Hash $githubartifact,
+  Optional[Hash] $packages = undef,
+  Optional[Hash] $githubartifact = undef,
 ) {
   case $facts['os']['family'] {
     "RedHat": {
-      ensure_resources("githubartifact::install", $githubartifact)
+      if $githubartifact {
+        ensure_resources("githubartifact::install", $githubartifact)
+      }
     }
     "Debian": {
-      $distro_codename = $facts['os']['distro']['codename']
-      apt::key { "2E6994033390DE82D8E6A2D16E187A4C6694369F":
-        source => "https://ose-repo.syslog-ng.com/apt/syslog-ng-ose-pub.asc",
-      }
-      -> apt::source { "syslog-ng-ose":
-        location => "https://ose-repo.syslog-ng.com/apt/",
-        release => "stable",
-        repos => "ubuntu-$distro_codename",
-        before => Package[keys($packages)],
-      }
-      ensure_resources(
-        package,
-        $packages,
-        {
-          ensure => present,
-          notify => Service["syslog-ng"],
+      if $packages {
+        $distro_codename = $facts['os']['distro']['codename']
+        apt::key { "2E6994033390DE82D8E6A2D16E187A4C6694369F":
+          source => "https://ose-repo.syslog-ng.com/apt/syslog-ng-ose-pub.asc",
         }
-      )
+        -> apt::source { "syslog-ng-ose":
+          location => "https://ose-repo.syslog-ng.com/apt/",
+          release => "stable",
+          repos => "ubuntu-$distro_codename",
+          before => Package[keys($packages)],
+        }
+        ensure_resources(
+          package,
+          $packages,
+          {
+            ensure => present,
+            notify => Service["syslog-ng"],
+          }
+        )
+      }
     }
   }
 }
